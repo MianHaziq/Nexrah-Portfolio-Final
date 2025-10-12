7b6598786832f2d246facf5bcc6d0b69bb8d3307
l1f21bsse0424 <l1f21bsse0424@ucp.edu.pk>
2025-10-12 20:21:01 +0500

    fix:fix first stagging issues -3

--- components/GridSection.tsx
+++ components/GridSection.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import MagicBento from './MagicBento'
+// import React from 'react'
+// import MagicBento from './MagicBento'
 
 
-const GridSection = () => {
-  return (
-    <>
-    <div className='w-full'>
+// const GridSection = () => {
+//   return (
+//     <>
+//     <div className='w-full'>
     
-    <MagicBento/>
-    </div>
-    </>
-  )
-}
+//     <MagicBento/>
+//     </div>
+//     </>
+//   )
+// }
 
-export default GridSection
+// export default GridSection
